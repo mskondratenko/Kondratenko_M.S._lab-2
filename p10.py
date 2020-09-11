49a4023bf4a5dd08a8842677bb6d0e10949f21f5
mskondratenko <70949270+mskondratenko@users.noreply.github.com>
2020-09-11 18:19:09 +0300

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,16 @@
+import turtle as t
+
+t.speed(1000)
+t.shape('turtle')
+def doublecircle():
+    for n in range(180):
+        t.forward(2)
+        t.right(2)
+    for n in range(180):
+        t.forward(2)
+        t.left(2)
+for n in range (3):
+    doublecircle()
+    t.right(60)
+
+
